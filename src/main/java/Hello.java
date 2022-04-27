75b2155ffa9e024b935b3a45cd4bd79ea5e572cd
Калачиков Максим Павлович <xvelx@rambler.ru>
2022-04-27 22:47:45 +0700

    Small "Hello" program added.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Hello {
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hi! What is your name?");
+        String name = new Scanner(System.in).next();
+        System.out.printf("Glad to see you, %s!", name);
+    }
 }
